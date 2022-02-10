ee8fbff253ad5ac9954eb8199e6e5ca93d207f94
tomaszpankowski <pankowski.email@gmail.com>
2022-02-10 15:53:04 +0100

    layout done

--- src/views/service.js
+++ src/views/service.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import ServiceS1 from "../components/service-s1";
+import ServiceS2 from "../components/service-s2";
+import ServiceS3 from "../components/service-s3";
 
 class Service extends Component{
     render(){
         return(        
-            <Container fluid className="service-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <ServiceS1/>
+                <ServiceS2/>
+                <ServiceS3/>
             </Container>    
         );
     }
